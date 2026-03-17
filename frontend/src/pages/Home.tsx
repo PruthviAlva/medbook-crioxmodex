f48673ea1d3cfb7573ff36b5c526f71f30a81d7f
Pruthvi Alva <alva.pruthvi@gmail.com>
2026-03-18 01:42:35 +0530

    frontend fix

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import API from "../services/api";
 import { useNavigate } from "react-router-dom";
 
